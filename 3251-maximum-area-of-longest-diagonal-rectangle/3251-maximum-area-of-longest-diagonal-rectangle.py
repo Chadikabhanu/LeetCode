6a665b27f3e6664e5273957445ffe52782743a6e
Bhanu <147791849+Chadikabhanu@users.noreply.github.com>
2025-08-26 09:50:47 +0530

    Time: 6 ms (8.05%), Space: 17.7 MB (89.78%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.py
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.py
@@ -3,11 +3,11 @@ class Solution:
         max_dia_sq = 0
         max_area = 0
         for l, w in dimensions:
-            dia_sq = l * l + w * w
+            d = l * l + w * w
             area = l * w
-            if dia_sq > max_dia_sq:
-                max_dia_sq = dia_sq
+            if d > max_dia_sq:
+                max_dia_sq = d
                 max_area = area
-            elif dia_sq == max_dia_sq:
+            elif d == max_dia_sq:
                 max_area = max(max_area, area)
         return max_area
