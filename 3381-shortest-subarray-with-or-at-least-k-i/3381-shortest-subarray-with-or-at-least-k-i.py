df2c9f4f6c7ed98496d9a42eed684cfa271e72c6
Bhanu <147791849+Chadikabhanu@users.noreply.github.com>
2025-10-22 06:13:16 +0530

    Time: 3 ms (85.12%), Space: 17.6 MB (100%) - LeetHub

--- 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.py
+++ 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.py
@@ -1,27 +1,12 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        bitCount = [0] * 32
-        def add(num):
-            for b in range(32):
-                if num & (1 << b):
-                    bitCount[b] += 1
-        def remove(num):
-            for b in range(32):
-                if num & (1 << b):
-                    bitCount[b] -= 1
-        def currentOR():
-            val = 0
-            for b in range(32):
-                if bitCount[b] > 0:
-                    val |= (1 << b)
-            return val
-        ans = float('inf')
-        l = 0
-        for r in range(n):
-            add(nums[r])
-            while l <= r and currentOR() >= k:
-                ans = min(ans, r - l + 1)
-                remove(nums[l])
-                l += 1
-        return ans if ans != float('inf') else -1
+        ans = inf
+        for left in range(n):
+            temp = 0
+            for right in range(left,n):
+                temp |= nums[right]
+                if temp>=k:
+                    ans = min(ans,right-left+1)
+                    break
+        return ans if ans!=inf else -1
